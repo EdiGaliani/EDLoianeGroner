0a654608c395a237b22b1007e626f8387e9deb0b
galiano <edigaliano@gmail.com>
2022-10-19 21:25:40 -0300

    First Study About Create Arrays

---
+++ 1º - Arrays/array/script.js
@@ -0,0 +1,14 @@
+let daysOfWeek = new Array();
+daysOfWeek = new Array(7);
+
+console.log(daysOfWeek);
+
+daysOfWeek = new Array('sunday', 'monday', 'tuesday', 'wednesday',
+                        'thursday', 'friday', 'saturday');
+console.log(daysOfWeek);
+
+// The Better Practice Way
+
+let daysOfWeek2 = [];
+
+console.log(daysOfWeek.length);
